2995fb4129439a68cef17cfcf96607413b5311cd
Ulyana Melnyk <ulianamelnyk@gmail.com>
2020-04-23 15:44:44 +0300

    added helper functions for color and font-family

---
+++ src/styles.js
@@ -0,0 +1,12 @@
+
+export const setColor = {
+    primaryColor: "#03DDDC",
+    mainWhite: "fff",
+    mainBlack: "#222",
+    mainGrey: "#ececec"
+}
+
+export const setFont = {
+    main: "font-family: 'Special Elite', cursive",
+    second: "font - family: 'Changa', sans- serif"
+}
