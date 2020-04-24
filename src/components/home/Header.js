81cec4e227c4e483810a769a69292370f6819fd9
Ulyana Melnyk <ulianamelnyk@gmail.com>
2020-04-24 15:52:51 +0300

    imported Hero component to Header

--- src/components/home/Header.js
+++ src/components/home/Header.js
@@ -1,11 +1,11 @@
 import React from 'react'
+import Hero from '../globals/Hero'
+// import aboutImg from '../../images/aboutBcg.jpg'
 
 const Header = () => {
     return (
-        <div>
-            Hello from header
-        </div>
+        <Hero />
     )
-}
+};
 
-export default Header
+export default Header;
