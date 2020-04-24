79376fd894e557fd824cc2bbed8ee8d0e9d47ed1
Ulyana Melnyk <ulianamelnyk@gmail.com>
2020-04-24 21:17:28 +0300

    fixed type error

--- src/styles.js
+++ src/styles.js
@@ -9,4 +9,20 @@ export const setColor = {
 export const setFont = {
     main: "font-family: 'Special Elite', cursive",
     second: "font - family: 'Changa', sans- serif"
-}
+};
+
+export const setFlex = ({x="center",y="center"}={}) => {
+    return `
+    display: flex;
+    align-items:${y};
+    justify-content:${x};
+    `;
+};
+
+export const setBackground = ({
+    img = "https://images.pexels.com/photos/3094799/pexels-photo-3094799.jpeg?auto=compress&cs=tinysrgb&h=750&w=1260", 
+color = "rgba(0,0,0,0)"
+} = {}) => {
+    return `background:linear-gradient(${color}, ${color}),
+url(${img}}) no-repeat fixed center/cover`;
+};
