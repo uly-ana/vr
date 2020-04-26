6179c0449868de9a1074533abdfdbb53ad8af62b
Ulyana Melnyk <ulianamelnyk@gmail.com>
2020-04-26 15:09:25 +0300

    added button to header component

--- src/components/home/Header.js
+++ src/components/home/Header.js
@@ -2,11 +2,16 @@ import React from 'react'
 import Hero from '../globals/Hero'
 import homeImg from '../../images/homeBcg.jpeg'
 import Banner from '../globals/Banner'
-
+import {PrimaryBtn} from '../globals/Buttons'
 const Header = () => {
     return (
         <Hero img={homeImg}>
-            <Banner greeting="Welcome to " title="VR Club" text="some more info about this club"/>
+            <Banner greeting="Welcome to " title="VR Club" text="some more info about this club">
+                <PrimaryBtn>
+                    View Details
+                </PrimaryBtn>
+            </Banner>
+            
         </Hero>
     );
 };
