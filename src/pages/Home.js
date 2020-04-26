67166fa3657840b2ea88e6b6624874b5acd38c86
Ulyana Melnyk <ulianamelnyk@gmail.com>
2020-04-26 19:24:27 +0300

    imported About to home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import Header from '../components/home/Header'
+import About from '../components/home/About'
 
  const Home = () => {
     return (
         <>
         <Header />
+        <About/>
         </>
     );
 };
