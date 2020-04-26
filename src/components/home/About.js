f699e3a2d0adf5533f392733fb238fe42c859a2e
Ulyana Melnyk <ulianamelnyk@gmail.com>
2020-04-26 19:57:16 +0300

    imported Section and Title to About page

--- src/components/home/About.js
+++ src/components/home/About.js
@@ -1,10 +1,14 @@
 import React from 'react'
+import Section from '../globals/Section'
+import Title from '../globals/Title'
 
 const About = () => {
     return (
-        <div>
-            hello from about
-        </div>
+        <>
+           <Section>
+              <Title title="about us"></Title> 
+          </Section> 
+        </>
     )
 }
 
