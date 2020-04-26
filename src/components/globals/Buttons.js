99e01558cf98084b7b0439e61ab560349901e01f
Ulyana Melnyk <ulianamelnyk@gmail.com>
2020-04-26 15:10:28 +0300

    added style for button

--- src/components/globals/Buttons.js
+++ src/components/globals/Buttons.js
@@ -1,7 +1,21 @@
 import styled from 'styled-components'
-import {setColoe, setRem, setLetterSpacing, setFonts} from '../../styles'
+import { setColor, setRem, setLetterSpacing, setTransition} from '../../styles'
 
 export const PrimaryBtn = styled.button`
-
-
+display: inline-block;
+color: ${setColor.primaryColor};
+background: transparent;
+border-color: ${setColor.mainBlue};
+text-transform: capitalize;
+padding: ${setRem(15)};
+margin-top: ${setRem(30)};
+font-size: ${setRem(16)};
+${setLetterSpacing(2)};
+border-radius: ${setRem(3)};
+box-shadow: 0 0 40px 40px transparent inset, 0 0 0 0 transparent;
+  -webkit-transition: ${setTransition};
+  ${setTransition};
+&:hover{
+box-shadow: 0 0 10px 0 ${setColor.mainBlue} inset, 0 0 10px 4px ${setColor.mainBlue};
+}
 `
