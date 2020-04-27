61b9ea59ff57b8dd5d624c8f231115cdffa40e24
Ulyana Melnyk <ulianamelnyk@gmail.com>
2020-04-27 14:41:13 +0300

    added data file

---
+++ src/components/home/data.js
@@ -0,0 +1,30 @@
+import card1 from "../../images/card1.jpeg";
+import card2 from "../../images/card2.jpeg";
+import card3 from "../../images/card3.jpeg";
+
+export default [
+  {
+    id: 1,
+    img: card1,
+    title: "weekdays",
+    info:
+      "The price is for 1 person.Session duration 60 minutes.It includes briefing and 50 minutes of playing time. Please arrive no earlier than 5 minutes before the session, and in case of delay, notify the administrator.",
+    price: 125
+  },
+  {
+    id: 2,
+    img: card2,
+    title: "weekends",
+    info:
+      "The price is for 1 person.Session duration 60 minutes.It includes briefing and 50 minutes of playing time. Please arrive no earlier than 5 minutes before the session, and in case of delay, notify the administrator.",
+    price: 250
+  },
+  {
+    id: 3,
+    img: card3,
+    title: "gift card",
+    info:
+      "VR club gift cards are a great gift for any holiday.The price is for 1 person.Session duration 60 minutes.It includes briefing and 50 minutes of playing time. Please arrive no earlier than 5 minutes before the session, and in case of delay, notify the administrator.",
+    price: 375
+  }
+];
