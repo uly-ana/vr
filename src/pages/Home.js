818f237ff4a2045e9a23a1412db8538c33a58efe
Ulyana Melnyk <ulianamelnyk@gmail.com>
2020-04-27 15:12:43 +0300

    imported cards to home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import Header from '../components/home/Header'
 import About from '../components/home/About'
+import Cards from '../components/home/Cards'
 
  const Home = () => {
     return (
         <>
         <Header />
         <About/>
+        <Cards/>
         </>
     );
 };
