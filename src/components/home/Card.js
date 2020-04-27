a71fad202296997f9a13c51d34f3990033ec8282
Ulyana Melnyk <ulianamelnyk@gmail.com>
2020-04-27 15:11:55 +0300

    created Card component

---
+++ src/components/home/Card.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import styled from 'styled-components'
+import {SmallBtn} from '../globals/Buttons'
+
+ const Card = () => {
+    return (
+        <div>
+            <SmallBtn>Add</SmallBtn>
+        </div>
+    )
+}
+
+export default Card;
